057e52b371b50d34f5bf5f778201f1a4cf290aeb
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2023-09-12 16:21:46 +0600

    All Complete

---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="flex items-center justify-center h-screen font-bold text-xl">
+      Coming Soon
+    </div>
+  );
+};
+
+export default About;
